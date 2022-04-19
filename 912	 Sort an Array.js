8754f2a074872eb7bfb1e421e908dd69280ff243
Mukhammadjon <kayumovmukhammadjon@gmail.com>
2022-04-19 22:39:31 +0500

    New Problem

---
+++ 912	 Sort an Array.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ function merge(arr1,arr2){
+    let results=[];
+    let i=0;
+    let j=0;
+    while(i<arr1.length && j<arr2.length){
+        if(arr2[j]>arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        }
+        else{
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while(i<arr1.length){
+        results.push(arr1[i]);
+            i++;
+    }
+        while(j<arr2.length){
+        results.push(arr2[j]);
+            j++;
+    }
+    return results;
+}
+var sortArray = function(nums) {
+    if(nums.length<=1) return nums;
+    let mid=Math.floor(nums.length/2);
+    let left=sortArray(nums.slice(0,mid));
+    let right=sortArray(nums.slice(mid));
+    return merge(left,right);
+};
